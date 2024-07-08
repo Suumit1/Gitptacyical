a53bdf575195565e8db772b57b4cd582948f4138
Suumit1 <159883593+Suumit1@users.noreply.github.com>
2024-07-08 11:34:44 +0530

    Update Git123.java

--- GitProject/src/Gitsecond/Git123.java
+++ GitProject/src/Gitsecond/Git123.java
@@ -5,6 +5,10 @@ public class Git123 {
 	public static void main(String[] args) {
 		System.out.println("I am doing second commit");
 
+	public void m1()
+		{
+		System.out.println("Hello code is commited");
+
 	}
 
 }
